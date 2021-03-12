f98165b9ab228f76a7f2cd22c8e6915750e3d54f
abhi <abhishekbkhemka@gmail.com>
2021-03-12 17:49:53 +0530

    first commit

---
+++ build/precache-manifest.984a337aa900fb3d01a6a349bf8ead92.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "abd4ab5a96a15e41952b",
+    "url": "/static/css/main.18f295d9.chunk.css"
+  },
+  {
+    "revision": "abd4ab5a96a15e41952b",
+    "url": "/static/js/main.abd4ab5a.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "8731b66c5d3ee0433c3d",
+    "url": "/static/css/2.ccd71b08.chunk.css"
+  },
+  {
+    "revision": "8731b66c5d3ee0433c3d",
+    "url": "/static/js/2.8731b66c.chunk.js"
+  },
+  {
+    "revision": "544ba26b223227040a39f49e41f84247",
+    "url": "/index.html"
+  }
+];
